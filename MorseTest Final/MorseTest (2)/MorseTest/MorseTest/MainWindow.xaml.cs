35344c8f0ee85505cb67dd93952428a5aa6d546c
JakeEH <33324632+JakeEH@users.noreply.github.com>
2018-10-09 11:14:39 +0100

    First upload

---
+++ MorseTest Final/MorseTest (2)/MorseTest/MorseTest/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Reflection;
+
+namespace MorseTest
+{
+
+ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MorseTranslator MorseTranslator = new MorseTranslator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpMorseDictionary();
+        }
+
+
+
+        /// <summary>
+        /// Allows the desired file to be opened and it's contents displayed on screen for decoding 
+        /// •	Read in a file containing the following single line of Morse code. 
+        /// </summary>
+        private void OpenFile()
+        {
+
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            //Only allow txt as that is the specified input
+            openFile.DefaultExt = ".txt";
+            openFile.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = openFile.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document 
+                string filename = openFile.FileName;
+                //show the directory & contents of the file
+                txtFileDir.Text = filename;
+                txtFileContents.Text = File.ReadAllText(filename);
+
+            }
+        }
+
+        /// <summary>
+        /// •	Place the plain English version and the translated Morse code version of the user inputted reply into a new file to be sent back to the candidate.
+        /// </summary>
+        private void SaveFile()
+        {
+            //add the plain text version
+            string fileText = txtUserPlainText.Text + Environment.NewLine + txtUserMorse.Text;
+            //only allow plain text
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, fileText);
+            }
+        }
+
+        /// <summary>
+        /// set up the dictionary for the users file input to be matched by
+        /// </summary>
+        private void SetUpMorseDictionary()
+        {
+            string fileContents ="";
+            string[] dictionary;
+            //used embedded resource for portabilty
+            var assembly = Assembly.GetExecutingAssembly();
+            //TODO handle file file not found feedback 
+            string resourceName = "MorseTest.dictionary_list.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                 fileContents = reader.ReadToEnd();
+            }
+
+            // Open dictionary document 
+            //each line has a return and newline, replace with just a newline to use split()
+            fileContents = fileContents.Replace("\r\n", "\n");       
+            dictionary = fileContents.Split('\n');
+
+            this.MorseTranslator.CreateMorseDictionary(dictionary);
+            
+        }
+
+        /// <summary>
+        /// read in the file contents from the txtbox and match each word
+        /// </summary>
+        /// <returns></returns>
+        private string LookupFileContents()
+        {
+            string decodedFileContents = "";
+            string fileContents = txtFileContents.Text;
+
+            foreach (string splitInputMorse in fileContents.Split('/'))
+            {
+                decodedFileContents += this.MorseTranslator.MorseToEnglish(splitInputMorse);
+              
+            }
+                return decodedFileContents;
+        }
+
+
+        private void btnEncode_Click(object sender, RoutedEventArgs e)
+        {
+            MorseTranslator morse = new MorseTranslator();
+            txtUserMorse.Text = morse.EncodePlainText(txtUserPlainText.Text.ToLower());
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //check if empty
+            SaveFile();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btnDecode_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO - display feedback for ambiguous words and multiple words mat
+            txtFileDecoded.Text = LookupFileContents();
+        }
+
+        //private string DecodeFileContents(String fileContents, Boolean isReverseSort)
+        //{
+        //    MorseTranslator morse = new MorseTranslator();
+        //    IEnumerable<string> morsePossibilities;
+        //    morse.Count = 0;
+        //    string decodedPossibilities = "";
+        //    foreach (string splitInputMorse in fileContents.Split('/'))
+        //    {
+        //        morsePossibilities = morse.DecodeMorse(splitInputMorse, isReverseSort);
+        //        var morseList = morsePossibilities.ToList();
+        //        using (Hunspell hunspell = new Hunspell("en_gb.aff", "en_gb.dic"))
+        //        {
+        //            foreach (string s in morseList)
+        //            {
+        //                if (hunspell.Spell(s))
+        //                {
+        //                    if (s.Length > splitInputMorse.Length / 4 && s.Length <= splitInputMorse.Length)
+        //                    {
+        //                        decodedPossibilities += s;
+        //                        decodedPossibilities += " ";
+        //                    }
+        //                }
+        //            }
+        //            //  decodedPossibilities = morseList.Where(t => hunspell.Spell(t)).ToList().FirstOrDefault();
+        //            // Process all your words here, don't create the Hunspell object for every word.
+        //        }
+        //    }
+        //    return decodedPossibilities.Trim();
+        //}
+
+    }
+}
---
+++ MorseTest Final/MorseTest (2)/MorseTest/MorseTest/MorseTranslator.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseTest
+{
+    class MorseTranslator
+    {
+        public int Count { get; set; }
+        private Dictionary<string, string> MorseDictionary = new Dictionary<string, string>();
+        /**
+         * By looping through all the KeyValuePair<TKey, TValue>'s in the dictionary (which will be a sizable performance hit if you have a number of entries in the dictionary)
+            Use two dictionaries, one for value-to-key mapping and one for key-to-value mapping (which would take up twice as much space in memory).    
+            Use Method 1 if performance is not a consideration, use Method 2 if memory is not a consideration.
+          **/
+
+        private Dictionary<char, string> CharToMorse = new Dictionary<char, string>()
+        {
+                                       {'a', ".-"},
+                                       {'b', "-..."},
+                                       {'c', "-.-."},
+                                       {'d', "-.."},
+                                       {'e', "."},
+                                       {'f', "..-."},
+                                       {'g', "--."},
+                                       {'h', "...."},
+                                       {'i', ".."},
+                                       {'j', ".---"},
+                                       {'k', "-.-"},
+                                       {'l', ".-.."},
+                                       {'m', "--"},
+                                       {'n', "-."},
+                                       {'o', "---"},
+                                       {'p', ".--."},
+                                       {'q', "--.-"},
+                                       {'r', ".-."},
+                                       {'s', "..."},
+                                       {'t', "-"},
+                                       {'u', "..-"},
+                                       {'v', "...-"},
+                                       {'w', ".--"},
+                                       {'x', "-..-"},
+                                       {'y', "-.--"},
+                                       {'z', "--.."},
+                                       {'0', "-----"},
+                                       {'1', ".----"},
+                                       {'2', "..---"},
+                                       {'3', "...--"},
+                                       {'4', "....-"},
+                                       {'5', "....."},
+                                       {'6', "-...."},
+                                       {'7', "--..."},
+                                       {'8', "---.."},
+                                       {'9', "----."}
+                                   };
+
+
+        private Dictionary<string, string> MorseToChar = new Dictionary<string, string>()
+            {
+                        { ".-", "a" },
+                        { "-...", "b" },
+                        { "-.-.", "c" },
+                        { "-..", "d" },
+                        { ".", "e" },
+                        { "..-.", "f" },
+                        { "--.", "g" },
+                        { "....", "h" },
+                        { "..", "i" },
+                        { ".---", "j" },
+                        { "-.-", "k" },
+                        { ".-..", "l" },
+                        { "--", "m" },
+                        { "-.", "n" },
+                        { "---", "o" },
+                        { ".--.", "p" },
+                        { "--.-", "q" },
+                        { ".-.", "r" },
+                        { "...", "s" },
+                        { "-", "t" },
+                        { "..-", "u" },
+                        { "...-", "v" },
+                        { ".--", "w" },
+                        { "-..-", "x" },
+                        { "-.--", "y" },
+                        { "--..", "z" },
+            };
+
+        /// <summary>
+        /// create a morse dictionary of the plain text words from the lexicon dictionary
+        /// </summary>
+        /// <param name="dictionary"></param>
+        public void CreateMorseDictionary(String[] dictionary)
+        {
+            foreach (string word in dictionary)
+            {
+                //if the english word has not been added, encode and add to the dict
+                if (!this.MorseDictionary.ContainsKey(word))
+                {
+                    this.MorseDictionary.Add(word, this.EncodePlainText(word));
+                }                
+            }
+        }
+
+        /// <summary>
+        /// return the english for the passed in morse
+        /// </summary>
+        /// <param name="morseWord"></param>
+        /// <returns></returns>
+        public string MorseToEnglish(string morseWord)
+        {
+            string englishWord = "";
+            Dictionary<string, string> morseWords = this.LookupMorse(morseWord);
+
+            //for multiple matches return all the found values
+            foreach (string lookedUpWord in morseWords.Keys)
+            {
+                englishWord += lookedUpWord;
+                englishWord += " ";
+            }
+
+            return englishWord;
+        }
+
+        /// <summary>
+        /// get the kvp where the morse word equals from the file
+        /// </summary>
+        /// <param name="morseWord"></param>
+        /// <returns></returns>
+        private Dictionary<string, string> LookupMorse(string morseWord)
+        {
+            return this.MorseDictionary.Where(morse => morse.Value == morseWord).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        /// <summary>
+        /// matches the character to it's morse equivalent, builds up the encoded string 
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public string EncodePlainText(string plainText)
+        {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char character in plainText.ToCharArray())
+            {
+                if (CharToMorse.ContainsKey(character))
+                {
+                    stringBuilder.Append(CharToMorse[character]);
+                }
+                // if char not found, will be a space
+                else
+                {
+                    stringBuilder.Append("/");
+                }
+            }
+            //will always have a final / appended so trim last char
+            if (stringBuilder.ToString().EndsWith("/"))
+            {
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            }
+           
+            return stringBuilder.ToString();
+        }
+
+
+        //public IEnumerable<string> DecodeMorse(string morse, Boolean isReverseSort)
+        //{
+        //    IEnumerable<string> query = new[] { "" };
+        //    if (isReverseSort)
+        //    {
+        //        query = DecodeMorse(morse, MorseToChar.Reverse().ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+        //    }
+        //    else
+        //    {
+        //        query = DecodeMorse(morse, MorseToChar);
+        //    }
+
+        //    return query;
+        //}
+
+        //finds every possible combination, would error out with out of memory exception
+        //private IEnumerable<string> DecodeMorse(string morse, Dictionary<String, String> wordDictionary)
+        //{
+        //    IEnumerable<string> query = new[] { "" };
+        //    while (Count < 10000000)
+        //    {
+        //        var letters =
+        //            wordDictionary
+        //                .Where(kvp => morse.StartsWith(kvp.Key))
+        //                .Select(kvp => new
+        //                {
+        //                    letter = kvp.Value,
+        //                    remainder = morse.Substring(kvp.Key.Length)
+        //                })
+        //                .ToArray();
+        //        if (letters.Any())
+        //        {
+
+        //            query =
+        //               from l in letters
+        //               from x in DecodeMorse(l.remainder, wordDictionary)
+
+        //               select l.letter + x;
+
+        //            Count++;
+        //            return query.ToArray();
+        //        }
+        //        else
+        //        {
+
+        //            return new[] { "" };
+
+        //        }
+        //    }
+        //    return query;
+        //}
+
+
+
+    }
+
+
+}
